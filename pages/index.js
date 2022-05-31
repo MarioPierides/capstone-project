fea76db2c61c06de0bc200b5d9ef3f968a46ca3f
Mario Pierides <mario.pierides@gmx.de>
2022-05-31 10:06:08 +0200

    Added textarea+calendar

--- pages/index.js
+++ pages/index.js
@@ -21,85 +21,113 @@ export default function Home() {
     const location = form.locations.value;
     const activity = form.activities.value;
     const ageGroup = getAgeGroup([...form.ageGroup]);
+    const beschreibung = form.beschreibung.value;
+    const kalender = form.kalender.value;
 
     setMeetings(allPrevMeetings => [
       ...allPrevMeetings,
-      { title, location, activity, ageGroup, id: nanoid() },
+      {
+        title,
+        location,
+        activity,
+        ageGroup,
+        beschreibung,
+        kalender,
+        id: nanoid(),
+      },
     ]);
   }
   return (
     <>
       <Title>Erstelle dein Treffpunkt</Title>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label>Titel</label>
-          <input type="text" name="title" placeholder="Titel" required />
-        </div>
-        <div>
-          <label>Standorte</label>
-          <select name="locations">
-            <option value="">Bitte auswählen</option>
-            <option value="stadtpark">Stadtpark</option>
-            <option value="elbe">Elbe</option>
-            <option value="alster">Alster</option>
-          </select>
-        </div>
-        <div>
-          <label>Aktivitäten</label>
-          <select name="activities">
-            <option value="auswahl">Bitte auswählen</option>
-            <option value="gassigehen">Gassi gehen</option>
-            <option value="sport">Sport</option>
-            <option value="otherthings">Sonstiges</option>
-          </select>
-        </div>
-        <div>
+        <section>
           <div>
-            <h4>Altersgruppe</h4>
+            <label>Titel</label>
+            <input type="text" name="title" placeholder="Titel" required />
+          </div>
+          <div>
+            <label>Standorte</label>
+            <select name="locations">
+              <option value="">Bitte auswählen</option>
+              <option value="stadtpark">Stadtpark</option>
+              <option value="elbe">Elbe</option>
+              <option value="alster">Alster</option>
+            </select>
+          </div>
+          <div>
+            <label>Aktivitäten</label>
+            <select name="activities">
+              <option value="auswahl">Bitte auswählen</option>
+              <option value="gassigehen">Gassi gehen</option>
+              <option value="sport">Sport</option>
+              <option value="otherthings">Sonstiges</option>
+            </select>
+          </div>
+        </section>
+        <section>
+          <h4>Altersgruppe</h4>
+          <div>
             <div>
-              <div>
-                <label htmlFor="welpen">Welpen</label>
-                <input
-                  type="checkbox"
-                  name="ageGroup"
-                  id="welpen"
-                  data-displayname="Welpen"
-                />
-              </div>
-              <div>
-                <label htmlFor="junghunde">Junghunde</label>
-                <input
-                  type="checkbox"
-                  name="ageGroup"
-                  id="junghunde"
-                  data-displayname="Junghunde"
-                />
-              </div>
-              <div>
-                <label htmlFor="erwachsen">Erwachsen</label>
-                <input
-                  type="checkbox"
-                  name="ageGroup"
-                  id="erwachsen"
-                  data-displayname="Erwachsen"
-                />
-              </div>
-              <div>
-                <label htmlFor="senior">Senior</label>
-                <input
-                  type="checkbox"
-                  name="ageGroup"
-                  id="senior"
-                  data-displayname="Senior"
-                />
-              </div>
+              <label htmlFor="welpen">Welpen</label>
+              <input
+                type="checkbox"
+                name="ageGroup"
+                id="welpen"
+                data-displayname="Welpen"
+              />
+            </div>
+            <div>
+              <label htmlFor="junghunde">Junghunde</label>
+              <input
+                type="checkbox"
+                name="ageGroup"
+                id="junghunde"
+                data-displayname="Junghunde"
+              />
+            </div>
+            <div>
+              <label htmlFor="erwachsen">Erwachsen</label>
+              <input
+                type="checkbox"
+                name="ageGroup"
+                id="erwachsen"
+                data-displayname="Erwachsen"
+              />
+            </div>
+            <div>
+              <label htmlFor="senior">Senior</label>
+              <input
+                type="checkbox"
+                name="ageGroup"
+                id="senior"
+                data-displayname="Senior"
+              />
             </div>
           </div>
-        </div>
+        </section>
+        <section>
+          <div>
+            <label>Beschreibung</label>
+            <textarea
+              text="aaa"
+              name="beschreibung"
+              placeholder="Text - Beschreibung"
+            />
+          </div>
+        </section>
+        <section>
+          <div>
+            <label>Kalender</label>
+            <input type="date" name="kalender" />
+          </div>
+        </section>
 
-        <div>
-          <StyledButton>ADD</StyledButton>
-        </div>
+        <section>
+          <div>
+            <StyledButton>ADD</StyledButton>
+          </div>
+        </section>
       </form>
 
       <ul>
@@ -118,6 +146,8 @@ export default function Home() {
                   );
                 })}
               </ul>
+              <p>{meeting.kalender}</p>
+              <p>{meeting.beschreibung}</p>
             </li>
           );
         })}
